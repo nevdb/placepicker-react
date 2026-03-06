f5b1dbbaa8ee303718c949a67fd6ddfaf7654b8c
nevena <dnevena27@gmail.com>
2026-03-06 15:58:44 +0200

    add ConfigureCounter, change Decrement and Increment functionalities

--- placepicker/src/components/counter/CounterContent.jsx
+++ placepicker/src/components/counter/CounterContent.jsx
@@ -30,14 +30,30 @@ export default function Counter({ initialCount }) {
     [initialCount],
   );
 
-  const [counter, setCounter] = useState(initialCount);
+  // const [counter, setCounter] = useState(initialCount);
+  const [counterChanges, setCounterChanges] = useState(() => [
+    { value: initialCount, id: Math.random() * 1000 },
+  ]);
+
+  const currentCounter = counterChanges.reduce(
+    (prevCounter, counterChange) => prevCounter + counterChange.value,
+    0,
+  );
 
   const handleDecrement = useCallback(function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
+    // setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [
+      { value: -1, id: Math.random() * 1000 },
+      ...prevCounterChanges,
+    ]);
   }, []);
 
   const handleIncrement = useCallback(function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
+    // setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevCounterChanges) => [
+      { value: 1, id: Math.random() * 1000 },
+      ...prevCounterChanges,
+    ]);
   }, []);
 
   return (
@@ -50,7 +66,7 @@ export default function Counter({ initialCount }) {
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
           Decrement
         </IconButton>
-        <CounterOutput value={counter} />
+        <CounterOutput value={currentCounter} />
         <IconButton icon={PlusIcon} onClick={handleIncrement}>
           Increment
         </IconButton>
--- placepicker/src/pages/Counter.jsx
+++ placepicker/src/pages/Counter.jsx
@@ -4,21 +4,26 @@ import logoImg from "../assets/counter-logo.png";
 
 import CounterContent from "../components/counter/CounterContent";
 // import Header from "./components/Header";
+import ConfigureCounter from "../components/counter/ConfigureCounter.jsx";
 
 export default function Counter() {
   log("<App /> rendered");
 
-  const [enteredNumber, setEnteredNumber] = useState(0);
   const [chosenCount, setChosenCount] = useState(0);
 
-  function handleChange(event) {
-    setEnteredNumber(+event.target.value);
+  // function handleChange(event) {
+  //   setEnteredNumber(+event.target.value);
+  // }
+
+  // function handleSetClick() {
+  //   setChosenCount(enteredNumber);
+  //   setEnteredNumber(0);
+  // }
+
+  function handleSetCount(newCount) {
+    setChosenCount(newCount);
   }
 
-  function handleSetClick() {
-    setChosenCount(enteredNumber);
-    setEnteredNumber(0);
-  }
   return (
     <>
       <header id="main-header">
@@ -30,27 +35,8 @@ export default function Counter() {
         <h1>React - Behind The Scenes</h1>
       </header>
       <main>
-        <section
-          id="configure-counter"
-          className="my-8 mx-auto p-0 rounded-md text-center flex gap-2 items-center justify-center border-1 border-teal-500"
-        >
-          <h2 className="my-2 text-base font-bold text-teal-500">
-            Set Counter
-          </h2>
-          <input
-            type="number"
-            onChange={handleChange}
-            value={enteredNumber}
-            className="w-16 text-center m-2 px-1 py-2 border border-teal-500 rounded bg-teal-950 text-teal-500 text-base"
-          />
-          <button
-            onClick={handleSetClick}
-            className="cursor-pointer bg-transparent text-teal-300 border-0 hover:text-teal-600 border hover:border-teal-600"
-          >
-            Set
-          </button>
-        </section>
-        <CounterContent initialCount={chosenCount} />
+        <ConfigureCounter onSet={handleSetCount} />
+        <CounterContent key={chosenCount} initialCount={chosenCount} />
       </main>
     </>
   );
